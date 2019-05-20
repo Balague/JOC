d94a0836cdac52bdb4eab41adca07fdc6c0a28a9
nilbayo <49233216+nilbayo@users.noreply.github.com>
2019-05-20 12:48:16 +0200

    L
    L

--- mapa_joc/Area2D.gd
+++ mapa_joc/Area2D.gd
@@ -1,21 +1,9 @@
 extends Area2D
 
-var escena_explosio = preload("res://Explosió.tscn")
-signal mor
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-func explota():
-	var nova_explosio = escena_explosio.instance()
-	nova_explosio.position = position
-	get_tree().get_root().get_node("Node2D").add_child(nova_explosio)
-	emit_signal('mor')
+func teletransporta():
+	position = Vector2(100,100)
 
 
 func _on_Area2D_body_entered(body):
-	explota()
+	teletransporta()
 
